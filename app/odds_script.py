e9fbc0fa3bc832b3c4c417068106661dacf3637b
sp7683 <108552251+sp7683@users.noreply.github.com>
2022-08-06 21:33:15 -0400

    slight modification to script and output

--- app/odds_script.py
+++ app/odds_script.py
@@ -5,6 +5,7 @@ import json
 from dotenv import load_dotenv
 from app import APP_ENV
 import os
+from datetime import datetime
 
 API_KEY = os.getenv("API_KEY")
 
@@ -17,18 +18,19 @@ def money_lines(sport=None):
         response = requests.get(url)
 #code adapted from web requests excercise in class
         response_lst = json.loads(response.text)
-        response_df = pd.DataFrame(response_lst)
-        response_df_five = response_df.head(5)
+        response_df_five  = pd.DataFrame(response_lst).head(5)
+        date_time_list = [datetime.strptime(time,'%Y-%m-%dT%H:%M:%SZ').date() for time in response_df_five["commence_time"]]
+        response_df_five["event_date"] = date_time_list
 #found above syntax at https://favtutor.com/blogs/list-to-dataframe-python
         bookmakers_lst = response_df_five["bookmakers"].tolist()
         with open('output.txt', 'w') as f:
             print("Moneyline for the Next "+str(len(bookmakers_lst))+" Games:", file=f)
             print("     ", file=f)
-            print("Game 1: Home",response_df_five["home_team"][0],"versus Away",response_df_five["away_team"][0],"-- Start Time in UTC:",response_df_five["commence_time"][0], file=f)
-            print("Game 2: Home",response_df_five["home_team"][1],"versus Away",response_df_five["away_team"][1],"-- Start Time in UTC:",response_df_five["commence_time"][1], file=f)
-            print("Game 3: Home",response_df_five["home_team"][2],"versus Away",response_df_five["away_team"][2],"-- Start Time in UTC:",response_df_five["commence_time"][2], file=f)
-            print("Game 4: Home",response_df_five["home_team"][3],"versus Away",response_df_five["away_team"][3],"-- Start Time in UTC:",response_df_five["commence_time"][3], file=f)
-            print("Game 5: Home",response_df_five["home_team"][4],"versus Away",response_df_five["away_team"][4],"-- Start Time in UTC:",response_df_five["commence_time"][4], file=f)
+            print("Game 1: Home",response_df_five["home_team"][0],"versus Away",response_df_five["away_team"][0],"-- Event Date:",response_df_five["event_date"][0], file=f)
+            print("Game 2: Home",response_df_five["home_team"][1],"versus Away",response_df_five["away_team"][1],"-- Event Date:",response_df_five["event_date"][1], file=f)
+            print("Game 3: Home",response_df_five["home_team"][2],"versus Away",response_df_five["away_team"][2],"-- Event Date:",response_df_five["event_date"][2], file=f)
+            print("Game 4: Home",response_df_five["home_team"][3],"versus Away",response_df_five["away_team"][3],"-- Event Date:",response_df_five["event_date"][3], file=f)
+            print("Game 5: Home",response_df_five["home_team"][4],"versus Away",response_df_five["away_team"][4],"-- Event Date:",response_df_five["event_date"][4], file=f)
             for game in bookmakers_lst:
                 print("     ", file=f)
                 print("     ", file=f)

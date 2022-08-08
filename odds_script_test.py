e66712e200219a4d1a05a1dc55ddec9e1967ff47
Lrich14 <lr1696@stern.nyu.edu>
2022-08-08 17:53:40 -0400

    updated testing code

--- odds_script_test.py
+++ odds_script_test.py
@@ -3,7 +3,9 @@
 from odds_script import money_lines
 
 def test_money_lines():
-    message = "Sorry, that's not a valid sport or there are no moneylines available. Please enter a major US sport and try again."
+    error_message = "Error"
+    
     assert money_lines(sport="americanfootball_nfl") == None
-    assert money_lines(sport=None) != message
+    assert money_lines(sport=None) != error_message
+
     
